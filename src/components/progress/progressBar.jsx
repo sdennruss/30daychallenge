b6b0650bf0d848574582f17ed4ecf1fc3fd92c43
sdennruss <58655302+sdennruss@users.noreply.github.com>
2020-05-09 14:09:16 +0800

    adding progressbar color if user reached 30 days

--- src/components/progress/progressBar.jsx
+++ src/components/progress/progressBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {CircularProgressbar} from "react-circular-progressbar"
+import {CircularProgressbar, buildStyles} from "react-circular-progressbar"
 import "react-circular-progressbar/dist/styles.css"
 import ProgressBarStyle from './styleProgress';
 
 const ProgressBar = ({value, item}) => {
 
   const percentage = (item === 0 ? 0 : value)
+  const completed = (value === 100 ? "green" : null)
 
 
   return (
@@ -13,8 +14,13 @@ const ProgressBar = ({value, item}) => {
     <h5>Check your Progress</h5>
     <ProgressBarStyle >
         <CircularProgressbar
+         className="CircularProgressbar-path"
           value={value}
-          text={`${percentage}%`}
+          text={`${percentage}% `}
+          styles={buildStyles ({
+            pathColor: completed,
+            textColor: completed
+          })}
           />
       </ProgressBarStyle>
       </React.Fragment>
